f4700b7a75e256052eed980def32dcff121b8fac
unknown <piperin@byu.edu>
2025-10-30 16:46:16 -0600

    All tests pass :D (Phase 4)

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -1,14 +1,17 @@
 package server;
 
+import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import io.javalin.http.Context;
 import service.GameService;
 import service.UserService;
 
+import java.util.Map;
+
 public class ClearHandler {
     private final UserService userService;
     private final GameService gameService;
-
+    private final Gson gson = new Gson();
     public ClearHandler(UserService userService, GameService gameService) {
         this.userService = userService;
         this.gameService = gameService;
@@ -19,10 +22,9 @@ public class ClearHandler {
             userService.clear();
             gameService.clear();
             ctx.status(200);
-            ctx.result("");
         } catch (DataAccessException e) {
             ctx.status(500);
-            ctx.result("Error clearing database: " + e.getMessage());
+            ctx.result(gson.toJson(Map.of("message", "Error: Internal server error")));
         }
     }
 
